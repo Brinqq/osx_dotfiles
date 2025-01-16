84f1b774551d57b4eb71c39386b27fe639b53fb1
brinq <andrewrdavis18@gmail.com>
2025-01-16 07:01:43 -0800

    getting ready to refactor lsp

--- nvim/lua/config/lsp/mason.lua
+++ nvim/lua/config/lsp/mason.lua
@@ -1,3 +1,10 @@
+require("mason").setup()
+require("mason-lspconfig").setup()
+
+local lspconfig = require("lspconfig")
+local m = require"mason-lspconfig"
+local opts = {}
+
 local defaultServers = {
  "clangd",
  "rust_analyzer",
@@ -7,13 +14,6 @@ local defaultServers = {
  "asm_lsp",
  "cssls"
 }
-
-require("mason").setup()
-require("mason-lspconfig").setup()
-
-local lspconfig = require("lspconfig")
-local m = require"mason-lspconfig"
-local opts = {}
 
 m.setup{
   ensure_installed = {
@@ -25,19 +25,11 @@ m.setup{
 for _, server in pairs(defaultServers) do opts = {
   on_attach = require("config.lsp.handler").on_attach,
   capabilities = require("config.lsp.handler").capabilities
-  }
+}
 
 server = vim.split(server, "@")[1]
 lspconfig[server].setup(opts)
 end
-
--- Manuel lsp server setups
-
-lspconfig.bashls.setup{
-  on_attach = require("config.lsp.handler").on_attach,
-  capabilities = require("config.lsp.handler").capabilities,
-  settings = {}
-}
 
 lspconfig.lua_ls.setup{
   on_attach = require("config.lsp.handler").on_attach,
@@ -48,7 +40,7 @@ lspconfig.lua_ls.setup{
         globals = { "vim" },
       },
       workspace = {
-        bibrary = vim.api.nvim_get_runtime_file("", true),
+        library = vim.api.nvim_get_runtime_file("", true),
       },
     },
   },
